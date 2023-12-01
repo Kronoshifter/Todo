0b1903712d774d2284d66bec30f1c57ce7be7d38
Kronos <Kronoshifter@gmail.com>
2023-12-01 06:12:56 -0700

    new styles and fucntionality on todo card

--- web/src/components/todo-card/todo-card.component.ts
+++ web/src/components/todo-card/todo-card.component.ts
@@ -1,19 +1,20 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core'
-import { CommonModule, formatDate } from '@angular/common'
+import { Component, Input } from '@angular/core'
+import { CommonModule } from '@angular/common'
 import { TodoTask } from '../../model/todo-task'
 import { MatCardModule } from '@angular/material/card'
 import { MatCheckboxModule } from '@angular/material/checkbox'
 import { FormsModule } from '@angular/forms'
 import { FaIconComponent } from '@fortawesome/angular-fontawesome'
-import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import { faAngleRight, faCircleXmark } from '@fortawesome/free-solid-svg-icons'
 import { MatChipsModule } from '@angular/material/chips'
-import { faCalendar } from '@fortawesome/free-regular-svg-icons'
+import { faCalendar, faCalendarPlus } from '@fortawesome/free-regular-svg-icons'
 import { MatRippleModule } from '@angular/material/core'
+import { MatTooltipModule } from '@angular/material/tooltip'
 
 @Component({
   selector: 'todo-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatCheckboxModule, FormsModule, FaIconComponent, MatChipsModule, MatRippleModule],
+  imports: [CommonModule, MatCardModule, MatCheckboxModule, FormsModule, FaIconComponent, MatChipsModule, MatRippleModule, MatTooltipModule],
   templateUrl: './todo-card.component.html',
   styleUrl: './todo-card.component.scss',
 })
@@ -22,11 +23,20 @@ export class TodoCardComponent {
 
   protected readonly faAngleRight = faAngleRight
   protected readonly faCalendar = faCalendar
+  protected readonly faCalendarPlus = faCalendarPlus
+  protected readonly faCircleXmark = faCircleXmark
 
   get dueDate(): Date | null {
     if (this.task.dueDate) {
-      return new Date(this.task.dueDate);
+      return new Date(this.task.dueDate)
     }
-    return null;
+    return null
+  }
+
+  setDueDate(task: TodoTask) {
+    // TODO: Add date picker
+    if (!task.dueDate) {
+      task.dueDate = Date.now()
+    }
   }
 }
