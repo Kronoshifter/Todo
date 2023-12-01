ea9f7dc097c09b128f9107bedb10e022ea88fda1
Kronos <Kronoshifter@gmail.com>
2023-12-01 06:47:23 -0700

    added drag and drop with animations to todo list

--- web/src/components/todo-list/todo-list.component.ts
+++ web/src/components/todo-list/todo-list.component.ts
@@ -2,11 +2,13 @@ import { Component, EventEmitter, Input, Output } from '@angular/core'
 import { CommonModule } from '@angular/common';
 import { TodoTask } from '../../model/todo-task'
 import { TodoCardComponent } from '../todo-card/todo-card.component'
+import { MatCardModule } from '@angular/material/card'
+import { CdkDrag, CdkDragDrop, CdkDragPlaceholder, CdkDragPreview, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop'
 
 @Component({
   selector: 'todo-list',
   standalone: true,
-  imports: [CommonModule, TodoCardComponent],
+  imports: [CommonModule, TodoCardComponent, MatCardModule, CdkDropList, CdkDrag, CdkDragPreview, CdkDragPlaceholder],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss'
 })
@@ -17,5 +19,9 @@ export class TodoListComponent {
 
   selectTask(task: TodoTask) {
     this.taskSelected.emit(task)
+  }
+
+  dropTask(event: CdkDragDrop<TodoTask>) {
+    moveItemInArray(this.tasks, event.previousIndex, event.currentIndex)
   }
 }
