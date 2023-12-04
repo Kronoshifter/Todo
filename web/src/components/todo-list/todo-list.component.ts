420b7876b2e0f85f774e59fabf8a098874e9b0f5
Kronos <Kronoshifter@gmail.com>
2023-12-04 14:34:42 -0700

    cleaned up imports

--- web/src/components/todo-list/todo-list.component.ts
+++ web/src/components/todo-list/todo-list.component.ts
@@ -1,16 +1,15 @@
-import { AfterContentInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
-import { CommonModule } from '@angular/common'
-import { TodoTask } from '../../model/todo-task'
-import { TodoCardComponent } from '../todo-card/todo-card.component'
-import { MatCardModule } from '@angular/material/card'
-import { CdkDrag, CdkDragDrop, CdkDragPlaceholder, CdkDragPreview, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop'
-import { SortByCompletePipe } from '../../pipes/sortByComplete.pipe'
-import { NgAutoAnimateDirective } from 'ng-auto-animate'
+import {Component, EventEmitter, Input, Output} from '@angular/core'
+import {CommonModule} from '@angular/common'
+import {TodoTask} from '../../model/todo-task'
+import {TodoCardComponent} from '../todo-card/todo-card.component'
+import {MatCardModule} from '@angular/material/card'
+import {SortByCompletePipe} from '../../pipes/sortByComplete.pipe'
+import {NgAutoAnimateDirective} from 'ng-auto-animate'
 
 @Component({
   selector: 'todo-list',
   standalone: true,
-  imports: [CommonModule, TodoCardComponent, MatCardModule, CdkDropList, CdkDrag, CdkDragPreview, CdkDragPlaceholder, SortByCompletePipe, NgAutoAnimateDirective],
+  imports: [CommonModule, TodoCardComponent, MatCardModule, SortByCompletePipe, NgAutoAnimateDirective],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss',
 })
@@ -21,10 +20,6 @@ export class TodoListComponent {
 
   constructor() {
 
-  }
-
-  selectTask(task: TodoTask, event: MouseEvent) {
-    this.select.emit(task)
   }
 
 }
